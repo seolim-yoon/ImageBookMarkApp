32620707832efd662c06aadd1d32e9dc8316d577
seolim-yoon <tolylim95@gmail.com>
2025-06-22 00:32:18 +0900

    - ViewerScreen 메모리 캐시 크기 조절

--- app/src/main/java/com/example/webtoonsearchapp/ui/viewer/screen/ViewerScreen.kt
+++ app/src/main/java/com/example/webtoonsearchapp/ui/viewer/screen/ViewerScreen.kt
@@ -4,10 +4,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.paging.compose.LazyPagingItems
+import com.bumptech.glide.Glide
+import com.bumptech.glide.MemoryCategory
 import com.example.webtoonsearchapp.ui.viewer.item.GlideImageItem
 import com.example.webtoonsearchapp.util.IMAGE_URL_ITEM_TYPE
 import kotlinx.coroutines.flow.filter
@@ -20,6 +24,7 @@ internal fun ViewerScreen(
     currentLinkUrl: String
 ) {
     val listState = rememberLazyListState()
+    val context = LocalContext.current
 
     LaunchedEffect(currentLinkUrl) {
         snapshotFlow { pagingList.itemSnapshotList.items }
@@ -31,18 +36,26 @@ internal fun ViewerScreen(
             }
     }
 
+    DisposableEffect(Unit) {
+        Glide.get(context).setMemoryCategory(MemoryCategory.HIGH)
+
+        onDispose {
+            Glide.get(context).setMemoryCategory(MemoryCategory.NORMAL)
+        }
+    }
+
     LazyColumn(
         state = listState,
         modifier = Modifier.fillMaxSize()
     ) {
         items(
+            key = { index -> pagingList[index] ?: index },
             contentType = { IMAGE_URL_ITEM_TYPE },
             count = pagingList.itemCount
         ) { index ->
+
             pagingList[index]?.let { imageUrl ->
-               GlideImageItem(
-                   imageUrl = imageUrl
-               )
+                GlideImageItem(imageUrl = imageUrl)
             }
         }
     }
